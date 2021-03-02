1d2893beeb8ce93eb110d6f1be64e6ca11a6486d
Joe Ho <joebcrmd@gmail.com>
2021-03-02 14:51:36 +0800

    add route constants

---
+++ src/app/constants/routes.js
@@ -0,0 +1,8 @@
+export const LANDING = "/";
+export const HOME = "/home";
+export const PROFILE = "/profile";
+export const JOURNAL = "/journal";
+export const CREATE_POST = "/create";
+export const EDIT_POST = "/edit";
+export const SIGN_IN = "/signin";
+export const SIGN_UP = "/signup";
