299a57f036117a67b9a7cb260cad2844abb48737
Joe Ho <joebcrmd@gmail.com>
2021-03-08 22:47:08 +0800

    add styles

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,55 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "constants/routes";
+import {
+  CreatePost,
+  EditPost,
+  Feed,
+  Journal,
+  Profile,
+  SignIn,
+  SignUp,
+  NotFound,
+} from "screens";
+import Navigation from "components/Navigation";
+import withAuthentication from "session/authUser";
+import PrivateRoute from "components/PrivateRoute";
+import AuthenticatedRoute from "components/AuthenticatedRoute";
+import Layout from "components/Layouts";
 
-const App = () => <div>hello world</div>;
+const App = () => (
+  <Router>
+    <Navigation />
+    <Layout>
+      <Switch>
+        <Route exact path={ROUTES.LANDING}></Route>
+        <PrivateRoute path={ROUTES.HOME}>
+          <Feed />
+        </PrivateRoute>
+        <AuthenticatedRoute path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </AuthenticatedRoute>
+        <AuthenticatedRoute path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </AuthenticatedRoute>
+        <PrivateRoute path={ROUTES.PROFILE}>
+          <Profile />
+        </PrivateRoute>
+        <PrivateRoute path={ROUTES.JOURNAL}>
+          <Journal />
+        </PrivateRoute>
+        <PrivateRoute path={ROUTES.CREATE_POST}>
+          <CreatePost />
+        </PrivateRoute>
+        <PrivateRoute path={ROUTES.EDIT_POST}>
+          <EditPost />
+        </PrivateRoute>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
+  </Router>
+);
 
-export default App;
+export default withAuthentication(App);
--- src/app/components/Layouts/index.js
+++ src/app/components/Layouts/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "./layout.module";
 
-const Layout = ({ children }) => <div>{children}</div>;
+const Layout = ({ children }) => (
+  <div className={styles.container}>
+    <div className={styles.content}>{children}</div>
+  </div>
+);
 
 Layout.propTypes = { children: PropTypes.node };
 
