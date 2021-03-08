aea6a0877770812af8df8c00ee731b1312159a12
Joe Ho <joebcrmd@gmail.com>
2021-03-08 22:47:35 +0800

    add firebase class

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "@/app/app";
+import App from "app/app";
+import Firebase, { FirebaseContext } from "firebaseUtils";
+import "normalize.css";
+import "styles/app";
 
 const root = document.getElementById("root");
 
-render(<App />, root);
+render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  root
+);
