3b9476d9e2478fd4d94b87a18e6315063f86da13
Joe Ho <joebcrmd@gmail.com>
2021-03-17 19:07:53 +0800

    add additional props to button

--- src/app/components/Button/index.js
+++ src/app/components/Button/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import style from "./button.module";
 
-const Button = ({ handleClick, text }) => {
+const Button = ({ handleClick, text, ...options }) => {
   return (
-    <button className={style.btn} type="button" onClick={handleClick}>
+    <button
+      className={style.btn}
+      type="button"
+      onClick={handleClick}
+      {...options}
+    >
       {text}
     </button>
   );
