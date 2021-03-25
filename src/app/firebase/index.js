364e35327a151682d10cdf5f4ec2c76745827155
Joe Ho <joebcrmd@gmail.com>
2021-03-25 18:26:05 +0800

    use environment variable for redirect url

--- src/app/firebase/index.js
+++ src/app/firebase/index.js
@@ -4,15 +4,14 @@ import "firebase/auth";
 import "firebase/storage";
 import "firebase/database";
 import { normalizeUser } from "utils";
-import { VERIFY_EMAIL_REDIRECT_URL } from "constants/firebase";
 
 const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 
 export const FirebaseContext = React.createContext(null);
@@ -59,7 +58,7 @@ class Firebase {
 
   doSendVerificationEmail() {
     return this.auth.currentUser.sendEmailVerification({
-      url: VERIFY_EMAIL_REDIRECT_URL,
+      url: process.env.FIREBASE_REDIRECT_URL,
     });
   }
 
