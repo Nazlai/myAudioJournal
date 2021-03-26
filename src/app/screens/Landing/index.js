324746d910022b4434a5aca16691042fe202bc05
Joe Ho <joebcrmd@gmail.com>
2021-03-26 22:30:30 +0800

    remove sign in link

--- src/app/screens/Landing/index.js
+++ src/app/screens/Landing/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Layout } from "components";
-import * as ROUTES from "constants/routes";
 
 const gitHubLink = "https://github.com/Nazlai/myAudioJournal";
 
@@ -10,10 +8,7 @@ const Landing = () => {
     <Layout>
       <h1>Hi There</h1>
       <p>
-        Please <Link to={ROUTES.SIGN_IN}>Sign in</Link> to continue
-      </p>
-      <p>
-        Be sure to check out the project <a href={gitHubLink}>Here</a>
+        Be sure to check out the project <a href={gitHubLink}>Here</a>.
       </p>
     </Layout>
   );
