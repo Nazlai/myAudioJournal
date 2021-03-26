fc70900ecc05cecc425744f24c9241473644013b
Joe Ho <joebcrmd@gmail.com>
2021-03-26 09:16:23 +0800

    add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
 const dotenv = require("dotenv").config();
@@ -73,7 +74,7 @@ const webpackConfig = {
         use: "url-loader",
       },
       {
-        test: /\.(ttf|eot|svg)$/i,
+        test: /\.(ttf|eot|svg|ico)$/i,
         use: "file-loader",
       },
     ],
@@ -87,7 +88,11 @@ const webpackConfig = {
     new webpack.DefinePlugin({
       "process.env": JSON.stringify(dotenv.parsed),
     }),
-    new HtmlWebpackPlugin({ template: "./src/app/index.html" }),
+    new FaviconsWebpackPlugin("./logo.png"),
+    new HtmlWebpackPlugin({
+      title: "my audio journal",
+      template: "./src/app/index.html",
+    }),
     new MiniCssExtractPlugin({
       filename: isDevelopment ? "[name].css" : "[name].[hash].css",
       chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
